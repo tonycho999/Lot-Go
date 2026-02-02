4e4c1efba43b4fcccfba8ccdacbf1f69dceb1d42
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-02 19:21:58 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,28 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = 'lotgo-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/firebase-config.js',
+  '/images/logo.png'
+];
+
+// 서비스 워커 설치 및 리소스 캐싱
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+
+// 리소스 요청 시 캐시 우선 전략
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
