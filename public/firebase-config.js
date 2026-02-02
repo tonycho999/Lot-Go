56f04408da6b235debaf57978f4fb208ba8db3df
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-02 18:31:10 +0800

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,11 @@
+// Lot-Go 프로젝트 전용 Firebase 설정값
+export const firebaseConfig = {
+  apiKey: "AIzaSyDvXGhasq7tSVUxpQjfvklzgxXRD29GuXk",
+  authDomain: "lot-go.firebaseapp.com",
+  databaseURL: "https://lot-go-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lot-go",
+  storageBucket: "lot-go.firebasestorage.app",
+  messagingSenderId: "397328831286",
+  appId: "1:397328831286:web:7005a3634a9426a1b5d997",
+  measurementId: "G-F46H492LBX"
+};
